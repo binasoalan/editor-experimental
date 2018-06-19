deae325e00feb7aaeaaae2cdc4bf9d9e220bc215
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-06-19 10:24:01 +0800

    Index not working

--- src/testcrap/core.cljs
+++ src/testcrap/core.cljs
@@ -36,18 +36,8 @@
   (let [st @state]
     {:value (om/db->tree query (get st key) st)}))
 
-(defmethod read :question/edit
-  [{:keys [query state]} _ {:keys [index]}]
-  (let [st @state]
-    {:value (om/db->tree query (nth (:questions st) index) st)}))
-
-(defmethod read :answers
-  [{:keys [query state]} key _]
-  (let [st @state]
-    {:value (om/db->tree query (get st key) st)}))
-
 (defmethod read :default
-  [{:keys [state]} key _]
+  [{:keys [state] :as env} key _]
   (let [st @state]
     (if (contains? st key)
       {:value (get st key)}
@@ -57,8 +47,9 @@
 (defmulti mutate om/dispatch)
 
 (defmethod mutate 'question/edit
-  [{:keys [state ref]} _ {:keys [text]}]
-  {:action #(swap! state update-in ref assoc :text text)})
+  [{:keys [state]} _ {:keys [text index]}]
+  (let [ref (nth (:questions @state) index)]
+    {:action #(swap! state update-in ref assoc :text text)}))
 
 (defmethod mutate 'answer/hodor
   [{:keys [state ref]} _ _]
@@ -69,8 +60,7 @@
   [:button
    {:on-click
     (fn [e]
-      (om/transact!
-       parent '[(answer/hodor)]))}
+      (om/transact! parent '[(answer/hodor)]))}
    "Hodor"])
 
 (defui Answer
@@ -102,8 +92,7 @@
 
   Object
   (render [this]
-    (let [{:keys [text
-                  answers]} (om/props this)]
+    (let [{:keys [text answers]} (om/props this)]
       (html
        [:div
         [:p "Question: " text]
@@ -112,60 +101,48 @@
 (def question (om/factory Question {:keyfn :id}))
 
 
-(defui QuestionEditor
-  static om/Ident
-  (ident [this {:keys [question/edit]}]
-    [:question/by-id (:id edit)])
+(defn question-text-input [form index question]
+  [:input
+   {:type "text"
+    :value (str (:text question))
+    :on-change
+    (fn [e]
+      (let [new-text (.. e -target -value)]
+        (om/transact!
+         form `[(question/edit {:text ~new-text
+                                :index ~index})])))}])
 
+(defn next-button [form index]
+  [:button
+   {:on-click
+    (fn [e]
+      (om/set-query!
+       form {:params {:index (inc index)}}))}
+   "Next"])
+
+
+(defui Form
   static om/IQueryParams
   (params [this]
     {:index 0})
 
   static om/IQuery
   (query [this]
-    [{'(:question/edit {:index ?index}) (om/get-query Question)}])
-
-  Object
-  (render [this]
-    (let [{:keys [question/edit]} (om/props this)
-          {:keys [text answers]} edit]
-      (html
-       [:div
-        [:input
-         {:type "text"
-          :value (str text)
-          :on-change
-          (fn [e]
-            (let [new-text (.. e -target -value)]
-              (om/transact!
-               this `[(question/edit {:text ~new-text})])))}]
-        [:button
-         {:on-click
-          (fn [e]
-            (om/update-query!
-             this update-in [:params :index] inc))}
-         "Next"]]))))
-
-(def question-editor (om/factory QuestionEditor))
-
-
-(defui Form
-  static om/IQuery
-  (query [this]
-    (into
-     [:id :name {:questions (om/get-query Question)}]
-     (om/get-query QuestionEditor)))
+    [:id :name {:questions (om/get-query Question)}])
 
   Object
   (render [this]
     (let [{:keys [name
-                  questions
-                  question/edit]} (om/props this)]
+                  questions]} (om/props this)
+          {:keys [index]}     (om/get-params this)
+          focused-question    (nth questions index)]
+      (println "index:" index)
       (html
        [:div
         [:p "Form: " name]
         [:ul (map question questions)]
-        (question-editor {:question/edit edit})]))))
+        (question-text-input this index focused-question)
+        (next-button this index)]))))
 
 
 (def reconciler
